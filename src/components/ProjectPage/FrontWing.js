c3c0231a2826e097743fabb01e7dba7e5affa825
Jared Cox <coxj1@mcmaster.ca>
2019-11-05 23:11:55 -0500

    Revert "some edits"
    This reverts commit b225150113d192d34f04f3ca25ff7ff0be126097.

--- src/components/ProjectPage/FrontWing.js
+++ src/components/ProjectPage/FrontWing.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import ProjectPage from './ProjectPage';
 
 import frontwingimg from './../../img/aerodynamics/new wing.png'
+import oncar from './../../img/front wing/standout.png';
 import fronwingcut from './../../img/front wing/shiny.png';
 import frontwingstream from './../../img/front wing/front-wing-streamlines.png';
 import frontwingiso from './../../img/front wing/iso.png';
@@ -17,8 +18,20 @@ export class FrontWing extends Component {
         return (
             <ProjectPage
             title="Front Wing Development and Analysis"
-            images={[frontwingimg, fronwingcut, frontwingstream, frontwingiso]}
-            description="" />
+            images={[
+                {image:frontwingimg,
+                description:"Front wing render"},
+                {image:oncar,
+                description:"Highlighted on car model"},
+                {image:fronwingcut,
+                description:"cut-away to dislay ribs and spar"},
+                {image:frontwingiso,
+                description:"Analyzed using iterations of CFD"},
+                {image:frontwingstream,
+                description:"Streamlines seen from the top"},
+                
+            ]}
+            description="The front wing is a crucial component in the aerodynamic package. Being the first piece that will interact with the air flow, how the wing manages the air will affect all other components of the aerodynamic package.  The design consisted of multiple iterations of modelling and CFD (computational fluid dynamics) to achieve the best possible results.  The main structure of the wing consists of two 'sandwiched' foils with an array of ribs and a single spar.  This design allows the component to be both lightweight and stiff to resist the different loads it will encounter on the track." />
         )
     }
 }
